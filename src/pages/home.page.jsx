032b7f105e2934110ce2fc3854d0a3d1105cf244
Janell Huyck <Janell.Huyck@outlook.com>
2023-05-24 19:10:38 -0400

    rough draft home page

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,92 +1,44 @@
 import React from 'react';
+import './home.page.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 
 function Home() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
-      <section id="resume">
-        <h2>Resume</h2>
-        <p>Here's a link to my resume: <a href="https://example.com/resume.pdf">Download</a></p>
-      </section>
-      <section id="projects">
-        <h2>Projects</h2>
-        <p>Here's a link to my personal projects page: <a href="https://example.com/projects">View</a></p>
-      </section>
-      <section id="blog">
-        <h2>Blog</h2>
-        <p>Here's a link to my blog: <a href="https://example.com/blog">Read</a></p>
-      </section>
-      <div className="btn btn-primary">Primary</div>
-      <h1>My Portfolio</h1>
-      <section id="resume">
-        <h2>Resume</h2>
-        <p>Here's a link to my resume: <a href="https://example.com/resume.pdf">Download</a></p>
-      </section>
-      <section id="projects">
-        <h2>Projects</h2>
-        <p>Here's a link to my personal projects page: <a href="https://example.com/projects">View</a></p>
-      </section>
-      <section id="blog">
-        <h2>Blog</h2>
-        <p>Here's a link to my blog: <a href="https://example.com/blog">Read</a></p>
-      </section>
-      <div className="btn btn-primary">Primary</div>
-      <h1>My Portfolio</h1>
-      <section id="resume">
-        <h2>Resume</h2>
-        <p>Here's a link to my resume: <a href="https://example.com/resume.pdf">Download</a></p>
-      </section>
-      <section id="projects">
-        <h2>Projects</h2>
-        <p>Here's a link to my personal projects page: <a href="https://example.com/projects">View</a></p>
-      </section>
-      <section id="blog">
-        <h2>Blog</h2>
-        <p>Here's a link to my blog: <a href="https://example.com/blog">Read</a></p>
-      </section>
-      <div className="btn btn-primary">Primary</div>
-      <h1>My Portfolio</h1>
-      <section id="resume">
-        <h2>Resume</h2>
-        <p>Here's a link to my resume: <a href="https://example.com/resume.pdf">Download</a></p>
-      </section>
-      <section id="projects">
-        <h2>Projects</h2>
-        <p>Here's a link to my personal projects page: <a href="https://example.com/projects">View</a></p>
-      </section>
-      <section id="blog">
-        <h2>Blog</h2>
-        <p>Here's a link to my blog: <a href="https://example.com/blog">Read</a></p>
-      </section>
-      <div className="btn btn-primary">Primary</div>
-      <h1>My Portfolio</h1>
-      <section id="resume">
-        <h2>Resume</h2>
-        <p>Here's a link to my resume: <a href="https://example.com/resume.pdf">Download</a></p>
-      </section>
-      <section id="projects">
-        <h2>Projects</h2>
-        <p>Here's a link to my personal projects page: <a href="https://example.com/projects">View</a></p>
-      </section>
-      <section id="blog">
-        <h2>Blog</h2>
-        <p>Here's a link to my blog: <a href="https://example.com/blog">Read</a></p>
-      </section>
-      <div className="btn btn-primary">Primary</div>
-      <h1>My Portfolio</h1>
-      <section id="resume">
-        <h2>Resume</h2>
-        <p>Here's a link to my resume: <a href="https://example.com/resume.pdf">Download</a></p>
-      </section>
-      <section id="projects">
-        <h2>Projects</h2>
-        <p>Here's a link to my personal projects page: <a href="https://example.com/projects">View</a></p>
-      </section>
-      <section id="blog">
-        <h2>Blog</h2>
-        <p>Here's a link to my blog: <a href="https://example.com/blog">Read</a></p>
-      </section>
-      <div className="btn btn-primary">Primary</div>
+    <div className="home-page-container">
+      <h1>Hi, I'm Janell!</h1>
+      <div className='home-page-content'>
+        <div className="home-page-upper-content">
+          <div>
+            <h2>Software Engineer</h2>
+            <ul>
+              <li>Ruby</li>
+              <li>Python</li>
+              <li>JavaScript</li>
+              <li>React</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Based in Cincinnati, OH</h3>
+          </div>
+          <a href="https://www.linkedin.com/in/janell-huyck/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedinIn} size="xl" />
+          </a>
+
+          <img className="headshot" src={require("../assets/images/ai-headshot2.jpeg")} alt="Janell" />
+        </div>
+        <div className="home-page-lower-content">
+          <p>
+          I like to think of myself as a Full Stack of awesomeness. With my background in Full Stack Web Development, 
+          I have the technical skills and know-how to build, test, and deploy software applications and features. 
+          But I also have a collaborative and supportive personality - I enjoy working with others, mentoring new team members, 
+          and bouncing ideas off each other. And as someone who's used to adapting to new situations - like switching from 
+          massage therapy to programming - I'm always ready to learn new things. For example, I used Chat GPT to help me 
+          craft this perfect "about" section!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
